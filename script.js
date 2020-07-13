ea74b7df5f86a80c4ba33cebd4d469f93b1e6a10
Anne Essien <rabbonette@gmail.com>
2020-07-13 13:49:11 +0100

    Add and remove the display class

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let media = window.matchMedia("(max-width: 1000px)");
 function openMenu() {
     if (media.matches) {
-        document.querySelector(".sidebar").style.display = "block";
+        document.querySelector("#sidebar").classList.add("display");
     }
 }
   
 function closeMenu() {
     if (media.matches) { 
-        document.querySelector(".sidebar").style.display = "none";
+        document.querySelector("#sidebar").classList.remove("display");
     } 
 }
 
 document.querySelector(".openbtn").addEventListener("click", openMenu);
-document.querySelector(".closebtn").addEventListener("click", closeMenu)
+document.querySelector("#sidebar").addEventListener("click", closeMenu)
